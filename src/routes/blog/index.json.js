97bb08b67e7e3a6818958f052f5118d5aa84ed8d
Mark Robson <mark.o.robson@gmail.com>
2020-07-27 13:05:15 +0100

    tweaks, fixes, adding some more images

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -1,17 +1,17 @@
-import posts from './_posts.js';
+import posts from './_posts.js'
 
-const contents = JSON.stringify(posts.map(post => {
-	return {
-		title: post.title,
-		intro: post.intro,
-		slug: post.slug
-	};
-}));
+const contents = JSON.stringify(posts.map(post => ({
+	title: post.title,
+	intro: post.intro,
+	publishDate: post.publishDate,
+	slug: post.slug
+}
+)))
 
 export function get(req, res) {
 	res.writeHead(200, {
 		'Content-Type': 'application/json'
-	});
+	})
 
-	res.end(contents);
+	res.end(contents)
 }
