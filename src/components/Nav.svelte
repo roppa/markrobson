62e5f16ac60c055f65ace79d41d742542d363a0d
Mark Robson <mark.o.robson@gmail.com>
2020-07-30 22:21:29 +0100

    added art glossary

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,20 +5,19 @@
     document.getElementById(this.dataset.target).classList.toggle("show");
     this.classList.toggle("collapsed");
   }
+
 </script>
 
-<nav
-  class="navbar navbar-expand-lg navbar-light bg-white custom-menu split-menu">
+<style>
+  a:hover {
+    cursor: pointer;
+  }
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-white custom-menu split-menu">
   <div class="container">
-    <button
-      class="navbar-toggler collapsed"
-      type="button"
-      data-toggle="collapse"
-      data-target="navbar-toggle-1"
-      aria-controls="navbar-toggle-1"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-      on:click={toggle}>
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="navbar-toggle-1"
+      aria-controls="navbar-toggle-1" aria-expanded="false" aria-label="Toggle navigation" on:click={toggle}>
       <span class="icon-bar top-bar" />
       <span class="icon-bar middle-bar" />
       <span class="icon-bar bottom-bar" />
@@ -28,36 +27,36 @@
     <div class="collapse navbar-collapse" id="navbar-toggle-1">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a
-            href="."
-            aria-current={segment === undefined ? 'page' : undefined}
+          <a href="." aria-current={segment===undefined ? 'page' : undefined}
             class="nav-link first-menu-item {!segment ? 'active' : ''}">
             home
           </a>
         </li>
 
         <li class="nav-item">
-          <a
-            href="about"
-            aria-current={segment === 'about' ? 'page' : undefined}
+          <a href="about" aria-current={segment==='about' ? 'page' : undefined}
             class="nav-link {segment === 'about' ? 'active' : ''}">
             about
           </a>
         </li>
 
         <li class="nav-item">
-          <a
-            href="blog"
-            aria-current={segment === 'blog' ? 'page' : undefined}
+          <a href="blog" aria-current={segment==='blog' ? 'page' : undefined}
             class="nav-link {segment === 'blog' ? 'active' : ''}">
             blog
           </a>
         </li>
 
+        <li class="nav-item dropdown">
+          <span class="nav-link first-menu-item dropdown-toggle" id="learning-dropdown" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">Learning</span>
+          <div class="dropdown-menu drop-to-right animated fadeIn fast" aria-labelledby="learning-dropdown">
+            <a class="dropdown-item" href="/glossary">Art Glossary</a>
+          </div>
+        </li>
+
         <li class="nav-item">
-          <a
-            aria-current={segment === 'contact' ? 'page' : undefined}
-            href="contact"
+          <a aria-current={segment==='contact' ? 'page' : undefined} href="contact"
             class="nav-link before-count {segment === 'contact' ? 'active' : undefined}">
             contact
           </a>
---
+++ src/routes/glossary.svelte
@@ -0,0 +1,87 @@
+<svelte:head>
+  <title>Art Glossary</title>
+</svelte:head>
+
+<section id="glossary" class="bg-white">
+  <div class="container">
+    <div class="row vcenter">
+      <div class="col-md-6 mb-3">
+        <h4 class="section-title mb-0">ART GLOSSARY</h4>
+        <div class="spacer spacer-line border-primary ml-0">&nbsp;</div>
+        <div class="spacer">&nbsp;</div>
+        <dl>
+
+          <dt>Ala Prima</dt>
+          <dd>wet on wet. A technique of layering wet paint on wet paint</dd>
+
+          <dt>Angle</dt>
+          <dd>two lines that intersect or join form an angle</dd>
+
+          <dt>Chiaroscuro</dt>
+          <dd>use of strong contrasts of light and dark</dd>
+
+          <dt>Colour</dt>
+          <dd>the property possessed by a paint which produces different sensations on the eye as a result of the way it
+            reflects or emits light</dd>
+
+          <dt>Contour/Outline</dt>
+          <dd>the visual outer shape of an object</dd>
+
+          <dt>Cool</dt>
+          <dd>colours that indicate a low temperature such as white, blue</dd>
+
+          <dt>Drawing</dt>
+          <dd>recording elements from nature whilst preserving their relationships of points, lines, and angles.
+            Observing, interpreting, intending, action of moving pencil on a medium, resulting in a reproduction of
+            what one observes on the medium</dd>
+
+          <dt>Edges</dt>
+          <dd>contrast of values. Not a line</dd>
+
+          <dt>Gesture</dt>
+          <dd>a suggestion of movement</dd>
+
+          <dt>Light</dt>
+          <dd>strong, sharp, diffused</dd>
+
+          <dt>Line</dt>
+          <dd>a mark between 2 points on a flat surface</dd>
+
+          <dt>Opaic (opacity)</dt>
+          <dd>the quality of not letting light through the medium</dd>
+
+          <dt>Perspective</dt>
+          <dd>a point of view</dd>
+
+          <dt>Point</dt>
+          <dd>a dot or mark (without dimension) on a drawing surface</dd>
+
+          <dt>Shape</dt>
+          <dd>the form of something</dd>
+
+
+          <dt>Tableau</dt>
+          <dd>a group of figures representing a scene from a story or from history</dd>
+
+          <dt>Transparancy</dt>
+          <dd>the quality of letting light through the medium</dd>
+
+          <dt>Value</dt>
+          <dd>relative lightness/darkness</dd>
+
+          <dt>Warm</dt>
+          <dd>colours that indicate a hot temperature such as strong yellow, orange, red</dd>
+
+        </dl>
+      </div>
+
+      <div class="col-md-6 feature-left">
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
